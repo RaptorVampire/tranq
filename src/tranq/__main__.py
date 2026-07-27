17e81632dec2c227e29dbffe7e5fcddd11e6b0e6
RaptorVampire <amirtest191919@gmail.com>
2026-07-28 01:58:43 +0330

    FIX Version

--- src/tranq/__main__.py
+++ src/tranq/__main__.py
@@ -5,8 +5,8 @@ def main():
     parser = argparse.ArgumentParser(description="tranq - Calm error handling for Python.")
     parser.add_argument("--version", action="version", version="tranq 0.2.1")
     parser.parse_args()
-    version="tranq 0.2.6"
-    print("tranq v0.2.6 - Calm error handling with advanced features.")
+    version="tranq 0.2.7"
+    print("tranq v0.2.7 - Calm error handling with advanced features.")
     print("Use @tranq.handle(...) or 'with tranq.retry(...):' in your code.")
 
 if __name__ == "__main__":
